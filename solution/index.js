8a3ad59c8ad471adb668137258ce2d4562f23a78
krylov123 <krylov123@gmail.com>
2021-10-15 16:15:37 +0300

    funny tests?

--- solution/index.js
+++ solution/index.js
@@ -1,11 +1,12 @@
 // solution/index.js
 module.exports = function (Homework) {
+    const {less,add} = Homework;
     return async (array, fn, initialValue, cb) => {
         const len = await new Promise(array.length);
         for (
-            let i = await new Promise((resolve) => Homework.add(0, 0, resolve));
-            await new Promise((resolve) => Homework.less(i, len, resolve));
-            i = await new Promise((resolve) => Homework.add(i, 1, resolve))
+            let i = await new Promise((resolve) => add(0, 0, resolve));
+            await new Promise((resolve) => less(i, len, resolve));
+            i = await new Promise((resolve) => add(i, 1, resolve))
         ) {
             const curValue = await new Promise((resolve) => array.get(i, resolve));
             initialValue = await new Promise((resolve) => fn(initialValue, curValue, i, array, resolve));
